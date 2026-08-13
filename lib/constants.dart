b27ce230ded005c1d1b4f5def7915617780df0ab
yokina <mail@cloxs.jp>
2026-08-14 07:28:28 +0900

    _

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 // シミュレータは 127.0.0.1。実機ならMacのLAN IPに置き換え
-const String kApiBase = 'https://y6dmxuksrkf3nxp4encz5ez2ua0joxlp.lambda-url.us-west-2.on.aws';
-// const String kApiBase = 'http://127.0.0.1:8000';
+// const String kApiBase = 'https://y6dmxuksrkf3nxp4encz5ez2ua0joxlp.lambda-url.us-west-2.on.aws';
+const String kApiBase = 'http://127.0.0.1:8000';
