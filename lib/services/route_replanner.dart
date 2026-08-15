6109365e1ac7f5eba70b67f21e5db01176daef16
yokina <mail@cloxs.jp>
2026-08-15 20:45:45 +0900

    RouteReplannerの公開APIを元の依存範囲に戻す

--- lib/services/route_replanner.dart
+++ lib/services/route_replanner.dart
@@ -5,8 +5,6 @@ import '../models/route_models.dart';
 import '../models/trip_models.dart';
 import 'route_search_service.dart';
 
-export '../logic/replan_anchor.dart';
-
 class RouteReplanRequest {
   final ReplanAnchor anchor;
   final String activeStepId;
