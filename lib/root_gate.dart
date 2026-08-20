37e2ae0a7b46f43052000ce070bd2c7bb23a0981
yokina <mail@cloxs.jp>
2026-08-21 08:51:56 +0900

    Gate group mode with city capability

--- lib/root_gate.dart
+++ lib/root_gate.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/root_tabs.dart';
 import 'pages/member_mode_page.dart';
 import 'providers/app_session_provider.dart';
+import 'providers/city_profile_provider.dart';
 
 class RootGate extends ConsumerWidget {
   const RootGate({super.key});
@@ -10,11 +11,17 @@ class RootGate extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appSession = ref.watch(appSessionProvider);
-    
+    final cityProfile = ref.watch(cityProfileProvider);
+
     if (appSession.isMemberMode) {
+      if (!cityProfile.capabilities.features.groupTrips) {
+        throw StateError(
+          'Member mode is not supported for APP_CITY=${cityProfile.key}',
+        );
+      }
       return const MemberModePage();
     }
-    
+
     return const RootTabs();
   }
 }
