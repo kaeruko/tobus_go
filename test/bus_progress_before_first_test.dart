73c11d70cce54275f192fc10867e5afe72a3af71
yokina <mail@cloxs.jp>
2026-08-30 14:33:53 +0900

    Fix package imports in bus progress tests

--- test/bus_progress_before_first_test.dart
+++ test/bus_progress_before_first_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:toei_go/models/bus_progress.dart';
-import 'package:toei_go/models/route_models.dart';
+import 'package:toeigo/models/bus_progress.dart';
+import 'package:toeigo/models/route_models.dart';
 
 void main() {
   StepSeg busStep() => StepSeg(
