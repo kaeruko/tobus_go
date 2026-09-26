0546e05fb791b955edabe8ddfbf8c273718bc065
yokina <mail@cloxs.jp>
2026-09-26 12:07:01 +0900

    Prepare providers for Riverpod 3 legacy APIs

--- lib/providers/route_search_provider.dart
+++ lib/providers/route_search_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../models/fare_models.dart';
