b50ffec730fea9d17544b9936adf23090b135a8f
yokina <mail@cloxs.jp>
2026-09-26 12:06:49 +0900

    Prepare providers for Riverpod 3 legacy APIs

--- lib/providers/active_trip_provider.dart
+++ lib/providers/active_trip_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import '../models/trip_models.dart';
 import '../services/trip_service.dart';
 
